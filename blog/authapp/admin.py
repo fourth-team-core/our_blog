e2fd68ecf995e0cc6ae7367952b8840a1e8f2658
RashidRysaev <rashid.rysaev93@gmail.com>
2022-07-24 11:44:51 +0200

    BlogUserAdmin extended in accordance with the BlogUser model

--- blog/authapp/admin.py
+++ blog/authapp/admin.py
@@ -9,6 +9,38 @@ class BlogUserAdmin(UserAdmin):
     add_form = BlogUserCreationForm
     form = BlogUserChangeForm
     model = BlogUser
-    list_display = ['email', 'username',]
+    list_display = ['username', 'first_name', 'last_name', 'email', 'is_author']
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        ('Personal info', {'fields': (
+            'username', 
+            'email', 
+            'first_name',
+            'last_name',
+            'password',
+            'is_deleted',
+        )}),
+        ('Permissions', {'fields': (
+            'is_admin', 
+            'is_moderator',
+            'is_superuser',
+            'is_staff',
+            'groups',
+            'user_permissions',
+        )}),
+        ('Important dates', {'fields': (
+            'last_login',
+            'date_joined',
+        )}),
 
-admin.site.register(BlogUser, BlogUserAdmin)
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_active')}
+        ),
+    )
+    search_fields = ('username', 'email',)
+    ordering = ('email',)
+
+admin.site.register(BlogUser, BlogUserAdmin) #
--- blog/authapp/forms.py
+++ blog/authapp/forms.py
@@ -8,11 +8,11 @@ class BlogUserCreationForm(UserCreationForm):
 
     class Meta:
         model = BlogUser
-        fields = ('username', 'email')
+        fields = ('first_name', 'last_name', 'username', 'email',)
 
 
 class BlogUserChangeForm(UserChangeForm):
 
     class Meta:
         model = BlogUser
-        fields = ('username', 'email')
+        fields = ('first_name', 'last_name', 'username', 'email',)
