34cb4b67b9b5304c01ae8725368a63853b8cc6e1
ZaitcevIlya <izaytsev.work@gmail.com>
2022-07-29 16:05:55 +0300

    fix broken auth, rework base template and split up it to parts

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -4,9 +4,9 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('auth/', include('authapp.urls')),
-    path('', TemplateView.as_view(template_name='mainapp/index.html'), name='index_url'),
-    path('login/', TemplateView.as_view(template_name='authapp/login1.html'), name='login_url'),
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    # path('', TemplateView.as_view(template_name='mainapp/index.html'), name='index_url'),
+    # path('login/', TemplateView.as_view(template_name='authapp/login1.html'), name='login_url'),
 ]
