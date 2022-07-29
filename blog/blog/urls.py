069359047aecebd6b5255f95a0431f2c3d6cc643
ZaitcevIlya <izaytsev.work@gmail.com>
2022-07-29 16:43:45 +0300

    fix registration form and delete redundant code

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('auth/', include('authapp.urls')),
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    # path('', TemplateView.as_view(template_name='mainapp/index.html'), name='index_url'),
-    # path('login/', TemplateView.as_view(template_name='authapp/login1.html'), name='login_url'),
 ]
