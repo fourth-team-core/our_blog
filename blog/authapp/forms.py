a31a5f406ed70126d018e6403058e7022701fcec
RashidRysaev <rashid.rysaev93@gmail.com>
2022-08-03 18:43:02 +0200

    Code cleanup with isort && black in order to achive flake8 demands

--- blog/authapp/forms.py
+++ blog/authapp/forms.py
@@ -1,36 +1,45 @@
-from django.forms import ValidationError
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordResetForm
+from django.contrib.auth.forms import (PasswordResetForm, UserChangeForm,
+                                       UserCreationForm)
+from django.forms import ValidationError
 
 from .models import BlogUser
 
 
 class BlogUserCreationForm(UserCreationForm):
-
     class Meta:
         model = BlogUser
-        fields = ('first_name', 'last_name', 'username', 'email')
+        fields = ("first_name", "last_name", "username", "email")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['email'].required = True
+        self.fields["email"].required = True
 
 
 class BlogUserChangeForm(UserChangeForm):
-
     class Meta:
         model = BlogUser
-        fields = ('first_name', 'last_name', 'username', 'email',)
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        )
 
 
 class EmailValidationOnForgotPassword(PasswordResetForm):
-
     def clean_email(self):
         """
         Check if the given email correlates with an existing user email.
         If this is not the case -> trow a proper error message.
         """
-        email = self.cleaned_data['email']
-        if not get_user_model().objects.filter(email__iexact=email, is_active=True).exists():
-            raise ValidationError("There is no user registered with the given email address!")
+        email = self.cleaned_data["email"]
+        if (
+            not get_user_model()
+            .objects.filter(email__iexact=email, is_active=True)
+            .exists()
+        ):
+            raise ValidationError(
+                "There is no user registered with the given email address!"
+            )
         return email
