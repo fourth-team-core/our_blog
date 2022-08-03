46da8be573462af3de25d346aa1f26c1b39e6530
RashidRysaev <rashid.rysaev93@gmail.com>
2022-08-03 18:23:54 +0200

    New views for password reset and change added; new templates added; email backend added; some minor changes implemented

--- blog/authapp/urls.py
+++ blog/authapp/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path, include
-from .views import BlogUserSignUpView, BlogUserLoginView, BaseUserProfileView
+from django.urls import path, reverse_lazy
+from .views import (
+    BlogUserSignUpView,
+    BlogUserLoginView,
+    BaseUserProfileView,
+    BaseUserPasswordChangeView,
+    BaseUserPasswordResetView,
+    )
 from django.contrib.auth import views as auth_views
 
 app_name = 'authapp'
@@ -9,4 +15,13 @@ urlpatterns = [
     path('login/', BlogUserLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', BaseUserProfileView.as_view(), name='profile'),
+    path('password_change/', BaseUserPasswordChangeView.as_view(), name='password-change'),
+    path('password_reset/', BaseUserPasswordResetView.as_view(), name='password-reset'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('authapp:password-reset-complete'), 
+        template_name='authapp/password_reset_confirm.html'), 
+        name='password-reset-confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='authapp/password_reset_complete.html'), 
+        name='password-reset-complete'),
 ]
