00411852884043bf0c904ac4b2cecc4932810175
johnk2280 <johnk2280@gmail.com>
2022-08-04 21:05:48 +0500

    [add] CommentAdmin, LikeAdmin

--- blog/mainapp/admin.py
+++ blog/mainapp/admin.py
@@ -6,20 +6,20 @@ from .models import Tag
 from .models import Comment
 from .models import Like
 
-from authapp.models import BlogUser
-from authapp.models import BlogUserProfile
 
-
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     list_display = ('name', 'url')
 
 
+@admin.register(PostCategory)
 class PostCategoryAdmin(admin.ModelAdmin):
     search_fields = ('url', 'name')
     list_display = ('id', 'url', 'name')
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     def display_author_name(self, obj: Post) -> str:
         return obj.author.username
@@ -27,23 +27,60 @@ class PostAdmin(admin.ModelAdmin):
     def display_post_category(self, obj: Post) -> str:
         return obj.category.name
 
-    # def display_tags(self, obj: Post) -> str:
-    #     return obj.tags
-
     display_author_name.short_description = 'Author'
     display_post_category.short_description = 'Post category'
-    # display_tags.short_description = 'Tags'
-
     list_display = (
         'id',
         'display_author_name',
         'display_post_category',
         'title',
-        # 'display_tags',
         'status',
     )
+    search_fields = (
+        'id',
+        'author__username',
+        'category__name',
+        'title',
+        'status',
+    )
+    list_filter = ('author__username', 'category__name', 'status')
 
 
-admin.site.register(PostCategory, PostCategoryAdmin)
-admin.site.register(Post, PostAdmin)
-admin.site.register(Tag, TagAdmin)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    def display_author_name(self, obj: Comment) -> str:
+        return obj.author.username
+
+    def display_post_title(self, obj: Comment) -> str:
+        return obj.post.title
+
+    display_author_name.short_description = 'Author'
+    display_post_title.short_description = 'Post title'
+    list_display = (
+        'display_author_name',
+        'display_post_title',
+        'parent_id',
+        'url',
+        'published_at',
+    )
+    search_fields = ('author__username', 'post__title', 'parent_id')
+    list_filter = ('author__username', 'post__title', 'parent_id')
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    def display_author_name(self, obj: Like) -> str:
+        return obj.author.username
+
+    def display_post_title(self, obj: Like) -> str:
+        return obj.post.title
+
+    display_author_name.short_description = 'Author'
+    display_post_title.short_description = 'Post title'
+
+    list_display = (
+        'display_author_name',
+        'display_post_title',
+    )
+    search_fields = ('author__username', 'post__title')
+    list_filter = ('author__username', 'post__title')
