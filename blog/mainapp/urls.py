bc39b6ef22e0f40604900140b45f150ee78b6b65
ZaitcevIlya <izaytsev.work@gmail.com>
2022-08-10 20:10:37 +0300

    Added users posts page and added link to profile. Added crud ops for posts for users. Refactoring and styling a bit.

--- blog/mainapp/urls.py
+++ blog/mainapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from mainapp.views import PostDetailView, PostCreateView
+from mainapp.views import PostDetailView, PostCreateView, UserPostsList, PostUpdateView, PostDeleteView
 
 app_name = "mainapp"
 
 urlpatterns = [
     path("<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("create/", PostCreateView.as_view(), name="post-create")
+    path("create/", PostCreateView.as_view(), name="post-create"),
+    path("update/<int:pk>/", PostUpdateView.as_view(), name="post-update"),
+    path("delete/<int:pk>/", PostDeleteView.as_view(), name="post-delete"),
+    path("users_posts/", UserPostsList.as_view(), name="user-posts")
 ]
--- blog/mainapp/views.py
+++ blog/mainapp/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from mainapp.models import Post
 
@@ -13,6 +14,21 @@ class PostsList(ListView):
         return Post.objects.filter(status='published').order_by('-published_at')
 
 
+class UserPostsList(ListView):
+    model = Post
+    template_name = "mainapp/users_posts_list.html"
+    context_object_name = "posts"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user).order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users_posts_page'] = True
+        return context
+
+
 class PostDetailView(DetailView):
     model = Post
     template_name = "mainapp/post_detail.html"
@@ -22,7 +38,19 @@ class PostDetailView(DetailView):
 class PostCreateView(CreateView):
     model = Post
     fields = ["title", "category", "url", "content", "tags", "status", "published_at"]
+    template_name_suffix = '_create_form'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ["title", "category", "url", "content", "tags", "status", "published_at"]
+    template_name_suffix = '_update_form'
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy("mainapp:user-posts")
